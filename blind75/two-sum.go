9bb01835f9348c129a37872a416cef34081e007d
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2026-06-26 13:41:28 +0530

    Two Sum Problem

---
+++ blind75/two-sum.go
@@ -0,0 +1,21 @@
+package blind75
+
+import "fmt"
+
+func twoSum(mySlice []int, target int) []int {
+	myMap := make(map[int]int)
+	for i, v := range mySlice {
+		if j, present := myMap[target-v]; present {
+			return []int{i, j}
+		} else {
+			myMap[v] = i
+		}
+	}
+	return []int{}
+}
+
+func OneTwoSum() {
+	mySlice, target := []int{0, -1, 2, -3, 1}, 1
+
+	fmt.Println("Requires Indeces Are", twoSum(mySlice, target))
+}
