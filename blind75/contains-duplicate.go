c8582b57f05503a9f6247ffdd2ff006cb8f9bb24
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2026-06-26 13:42:43 +0530

    Contains Duplicate Problem

---
+++ blind75/contains-duplicate.go
@@ -0,0 +1,23 @@
+package blind75
+
+import "fmt"
+
+func containsDuplicate(nums []int) bool {
+	mySet := make(map[int]struct{})
+
+	for _, data := range nums {
+		if _, present := mySet[data]; present {
+			return true
+		} else {
+			mySet[data] = struct{}{}
+		}
+	}
+
+	return false
+}
+
+func ThreeContainsDuplicate() {
+	nums := []int{1, 2, 3, 1}
+
+	fmt.Println("Contains Dupicate - ", containsDuplicate(nums))
+}
