8093e5381ff05f3fd61fb9fef0b15073129d3531
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2026-06-26 13:42:01 +0530

    Stock Buy and Sell Problem

---
+++ blind75/stock-buy-sell.go
@@ -0,0 +1,18 @@
+package blind75
+
+import "fmt"
+
+func maxProfit(prices []int) int {
+	profit, minPrice := 0, prices[0]
+	for price := range prices {
+		minPrice = min(minPrice, price)
+		profit = max(profit, price-minPrice)
+	}
+	return profit
+}
+
+func TwoStockBuySell() {
+	prices := []int{7, 1, 5, 3, 6, 4}
+
+	fmt.Println("Maximum Profit is", maxProfit(prices))
+}
